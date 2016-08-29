9e8aff1c6f7d7eb776dda79216ebd09b4cbccd19
Naoya Ikeda <naoya.ikeda@e-brainchild.com>
2016-08-29 19:22:54 +0900

    Initial Commit

---
+++ GsClock/js/main.js
@@ -0,0 +1,11 @@
+﻿var hourHand = document.getElementById("idHourHand");
+var minuteHand = document.getElementById("idMinuteHand");
+setInterval(function () {
+    var now = new Date();
+    var hour = now.getHours() % 12;
+    var minute = now.getMinutes();
+    var hourDeg = 360 * (hour / 12.0);
+    var minuDeg = 360 * (minute / 60.0);
+    hourHand.setAttribute("transform", "rotate(" + hourDeg + " 160 160)");
+    minuteHand.setAttribute("transform", "rotate(" + minuDeg + " 160 160)");
+}, 1);
